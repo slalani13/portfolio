3b2ebd3c05205fb6839008809c86a0e4ae7bb3e0
Shariq Lalani <slalani13@gmail.com>
2024-11-07 22:40:29 -0600

    updated about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -20,13 +20,14 @@ export default function About() {
                   </h2>
               </div>
               <p className="text-gray-300 text-center lg:text-left">
-              Hi there! I'm a software engineer with a passion for building data-driven solutions and software applications. My past experience includes software development, data analytics, and project management. 
+              From building resilient backend systems to creating seamless full-stack applications and advanced data pipelines, I’m passionate about crafting technology that moves the needle. Recently, I developed an AI-driven analytics framework at HrHouz, revolutionizing candidate reference checks by transforming raw data into actionable insights for faster hiring decisions. Imagine cutting down hours of analysis with a few clicks—that’s the impact I aim for!
+
               </p>
               <p className="text-gray-300 text-center lg:text-left">
-              Most recently, I graduated with a computer science master's degree from the University of Pennsylvania. While completing my degree, I was a software engineering intern at Hrhouz working on an AI-driven analytics service to support employers in the recruiting process and a graduate teaching assistant helping students with software development in Python and Java!
+              With a robust toolkit (Python, Flask, Java, JavaScript, C/C++, Node.js, React, SQL/NoSQL, AWS) and hands-on experience in software systems and analytics, I’m well-versed in designing solutions that deliver. While earning my master’s in Computer Science from the University of Pennsylvania, I also served as a graduate TA for Intro to Software Development, honing my ability to simplify complex concepts and mentor budding engineers.
               </p>
               <p className="text-gray-300 text-center lg:text-left">
-              Outside of work, I enjoy badminton, volleyball, board games, and karaoke!
+              Outside of work, I’m just as competitive, whether it’s on the badminton court, in volleyball matches, or over epic board games with friends. If you’re ready to talk code, strategy, or maybe just exchange game tactics, reach out— I'd love to connect!
               </p>
               <p className="text-gray-300 text-center lg:text-left">
                 Feel free to take a look at my resume
