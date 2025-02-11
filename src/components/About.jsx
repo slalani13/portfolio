532ac79fb7dfbc47230af2bf4281162979bd4c52
Shariq Lalani <slalani13@gmail.com>
2025-02-11 10:07:26 -0600

    fetched bio and profile

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,41 @@
+import {useState, useEffect} from "react"
 import headshot from "../assets/headshot.jpg";
 
 export default function About() {
+    const [profilePhoto, setProfilePhoto] = useState('');
+    const [bio, setBio] = useState('');
+  
+    useEffect(() => {
+      async function fetchUserProfile() {
+        const username = 'slalani13';
+        const GITHUB_TOKEN = import.meta.env.VITE_GITHUB_TOKEN;
+  
+        try {
+          const response = await fetch(`https://api.github.com/users/${username}`, {
+            method: 'GET',
+            headers: {
+              'Authorization': `Bearer ${GITHUB_TOKEN}`,  // Optional, if using authentication
+              'Accept': 'application/vnd.github.v3+json',
+              'X-GitHub-Api-Version': '2022-11-28',
+            },
+          });
+  
+          if (response.ok) {
+            const data = await response.json();
+            setProfilePhoto(data.avatar_url); // Profile photo URL
+            setBio(data.bio); // User's bio
+          } else {
+            console.error('Error fetching user data:', response.statusText);
+          }
+        } catch (error) {
+          console.error('Error fetching user data:', error);
+        }
+      }
+  
+      fetchUserProfile();
+    }, []);
+
+
   return (
       <section
           id="about"
@@ -8,7 +43,7 @@ export default function About() {
                      align-center bg-gray-900 max-w-5xl mx-auto text-gray-100 mt-10">
           <div className="flex-1 flex justify-center items-center">
               <img 
-                  src={headshot}  // Placeholder image
+                  src={profilePhoto}  // {headshot}
                   alt="About"
                   className="w-80 h-80 object-cover rounded-full border border-teal-400"
               />
@@ -19,6 +54,9 @@ export default function About() {
                       About Me
                   </h2>
               </div>
+              <p className="text-gray-300 text-center lg:text-left">
+                {bio}
+              </p>
               <p className="text-gray-300 text-center lg:text-left">
               From building resilient backend systems to creating seamless full-stack applications and advanced data pipelines, I’m passionate about crafting technology that moves the needle. Recently, I developed an AI-driven analytics framework at HrHouz, revolutionizing candidate reference checks by transforming raw data into actionable insights for faster hiring decisions. Imagine cutting down hours of analysis with a few clicks—that’s the impact I aim for!
 
